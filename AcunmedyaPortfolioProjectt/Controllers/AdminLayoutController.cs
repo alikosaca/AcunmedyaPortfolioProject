6d6fa118080aa7a4e8ced844eca4bc708c5ec8dc
ali <137689491+alikosaca@users.noreply.github.com>
2025-03-05 23:32:52 +0300

    Add project files.

---
+++ AcunmedyaPortfolioProjectt/Controllers/AdminLayoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunmedyaPortfolioProjectt.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PartialHead()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialMenu()
+        {
+            return PartialView();
+        }
+        public IActionResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ AcunmedyaPortfolioProjectt/Controllers/StaticController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunmedyaPortfolioProjectt.Controllers
+{
+	public class StaticController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
